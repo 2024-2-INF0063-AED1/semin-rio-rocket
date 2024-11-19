00c289e8d691d84f956423806f9d78117a05c2de
Diogo Quintão <quintaodiogo@gmail.com>
2024-11-19 18:55:48 -0300

    Modificado a struct para: - Trocar o campo 'status' por 'boleando', representando um valor booleano. - Adicionar um novo campo 'id' nas bibliotecas, para poder adicionar vários livros do mesmo

--- src/biblioteca.h
+++ src/biblioteca.h
@@ -4,16 +4,19 @@
 #define MAX_LIVROS 100
 
 typedef struct {
+    int id;
     char nome[100];
     char autor[100];
     char categoria[100];
     int ano;
-    char status[100];
+    bool status; // 1 para disponível, 0 para emprestado
 } Livro;
 
+// Declaração do array de livros e contador global
 extern Livro biblioteca[MAX_LIVROS];
 extern int contadorLivros;
 
+// Funções para manipulação da biblioteca
 void adicionarLivro();
 void removerLivro();
 void listarLivros();
